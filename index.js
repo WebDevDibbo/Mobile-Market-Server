7470eff2be4589dc5ac4329339d1b3f1878b97b7
dibbodas12 <dibbodas228@gmail.com>
2022-11-24 22:40:14 +0600

    categories api added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,28 @@ const app = express();
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gkejsh2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+       const mobileCategories = client.db('mobilemarket').collection('mobileCategories');
+
+       app.get('/categories',async(req,res)=>{
+        const query = {};
+        const result = await mobileCategories.find(query).toArray();
+        res.send(result);
+       })
+     }
+    finally{
+
+    }
+
+}
+run().catch(console.log())
 
 app.get('/',async(req,res)=>{
     res.send('mobile market server is running')
