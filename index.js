ac1ef034ed88da1d8a48d0e79be15b8fb15085b5
dibbodas12 <dibbodas228@gmail.com>
2022-11-27 10:20:39 +0600

    booking specific api added

--- index.js
+++ index.js
@@ -44,6 +44,19 @@ async function run(){
         console.log(result)
         res.send(result)
        })
+       app.get('/bookings',async(req,res)=>{
+        const email = req.query.email;
+        const query = {email : email}
+        const result = await bookingsCollection.find(query).toArray();
+        res.send(result);
+       })
+       app.get('/bookings/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : ObjectId(id)};
+        const booking = await bookingsCollection.findOne(query);
+        res.send(booking)
+       })
+
        app.post('/bookings',async(req,res)=>{
          
         const booking = req.body;
